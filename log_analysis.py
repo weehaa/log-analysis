fc1cc8bff0b1a7eae5d4edfd4cb1adaf9f85f172
weehaa <vzg1000@mail.ru>
2017-06-15 00:29:37 +0400

    add topArticles method

--- log_analysis.py
+++ log_analysis.py
@@ -18,4 +18,21 @@ def connect(database_name="news"):
         print("ERROR: Can't connect to {} database".format(database_name))
         quit()
 
-        
+
+def topArticles(limit=3):
+    """ Returns sorted list of `limit` popular articles
+    with the most popular article at the top."""
+
+    db, cursor = connect()
+
+    query = """SELECT a.title, count(l.path) views FROM articles a
+               LEFT JOIN log l ON l.path = '/article/'||a.slug
+               GROUP BY a.title
+               ORDER BY views DESC
+               LIMIT (%s)"""
+    params = (limit,)
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+
+    db.close()
+    return rows
